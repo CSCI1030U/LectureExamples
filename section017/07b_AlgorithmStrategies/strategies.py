90c9943cb4b63001a80eb1ef9307c824003c61b0
ExperimentsInCS <randy.fortier@gmail.com>
2020-11-10 17:01:10 -0500

    Added algorithm strategies demos (section 017)

--- section017/07b_AlgorithmStrategies/strategies.py
+++ section017/07b_AlgorithmStrategies/strategies.py
@@ -23,3 +23,37 @@ def binary_search(x, a, start, end):
 
 values = [-8, -2, 0, 3, 5, 10, 22, 24]
 print(binary_search(2, values, 0, len(values) - 1))
+
+def terrible_fibonacci(n):
+    if n <= 1:
+        return n 
+    
+    return terrible_fibonacci(n - 1) + terrible_fibonacci(n - 2)
+
+def better_fibonacci(n):
+    solutions = [0, 1]
+    if n <= 1:
+        return solutions[n]
+    
+    for i in range(2, n + 1):
+        solutions.append(solutions[i - 1] + solutions[i - 2])
+    
+    return solutions[n]
+
+print(better_fibonacci(100))
+
+marks_dictionary = [('100000001', 67.5), ('100000002', 70.0)]
+marks_dictionary.append(('100000003', 75.5))
+print(marks_dictionary)
+
+sid_to_find = '100000003'
+for sid, mark in marks_dictionary:
+    if sid == sid_to_find:
+        print(mark)
+
+'''
+float[] marks = ...;
+cout << marks[3];
+
+marks + (3 * size_of_int)
+'''
