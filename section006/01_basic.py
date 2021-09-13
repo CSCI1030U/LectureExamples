48fc2e59062cbe6b5cfabeb617e1246761f9070d
randyfortier <randy.fortier@uoit.ca>
2021-09-13 14:47:28 -0400

    Added code from sections 017 and 006

--- section006/01_basic.py
+++ section006/01_basic.py
@@ -1,2 +1,22 @@
 # 01a - Basic Programming and Debugging
 
+recipe_title = "Randy's Recipe"
+# recipeTitle = 'Randy\'s Recipe'
+
+print(recipe_title)
+
+# int x = 9;
+# int y = 13;
+# int temp = x;
+# x = y;
+# y = temp;
+
+# x = 9
+# y = 13
+x,y = 9,13
+
+x,y = y,x
+
+x = x + 1
+x += 1
+# x++    Not in Python :(
