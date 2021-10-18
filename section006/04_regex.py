92d8b53ccd8170af376729bf0ec7e54baae55c04
randyfortier <randy.fortier@uoit.ca>
2021-10-18 14:00:17 -0400

    regex examples for sections 017 and 006

--- section006/04_regex.py
+++ section006/04_regex.py
@@ -1,2 +1,58 @@
 # regular expressions
 
+# a|b        => a, b
+# (a|b)(a|b) => aa, ab, ba, bb
+# (a|b)*     => <blank>, a, b, aa, ab, ba, bb, aaa, ...
+# (a|b)*.*(a|b)* => <blank>?!aaab, a!9bb, aaa, b7654!bb, ...
+# (a|b)+         => a, b, aa, ab, ba, bb, aaa, aab, aba, ...
+# (a|b)?c+       => acccc, bc, cccc
+# (a|b){4}       => aaaa, aaab, aaba, ...
+# (a|b){2,3}     => aa, ab, ba, bb, aaa, aab, aba, ...
+# (0|1|2|3|4|5|6|7|8|9)
+# [0123456789]
+# [0-9]
+# [a-zA-Z]
+# [ab]
+# \.*  => <blank>, ., .., ..., .... (etc)
+# [?.] => ?, .
+# [^.,;:'"]     (any non-punctuation character)
+# [^0-9]        (any non-digit character)
+# [^a-zA-Z]     (any non-letter character)
+# \s            (space, tab, newline, equivalent to [ \t\n])
+# \w            ("word character", equivalent to [a-zA-Z0-9_])
+# \d            (digit character, [0-9])
+# [a-zA-Z_]\w*         (any variable name)
+# [a-z_][a-z0-9_]*     (snake case variable name)
+# [a-zA-Z][a-zA-Z0-9]* (camel case variable name)
+
+import re 
+
+# finding a single match
+number_regex = re.compile('[0-9]+')
+number_match = number_regex.search('there were 341 little pigs')
+print(number_match)
+if number_match:
+    print(f'String starts at index {number_match.start()}')
+    print(f'String ends at index {number_match.end()}')
+    print(f'String found is {number_match.group()}')
+else:
+    print('No match')
+
+# finding multiple matches
+review = "I bought this product for $100.  It wasn't even worth $1."
+price_regex = re.compile('\$[0-9]+')
+matches = price_regex.findall(review)
+print(f'matches = {matches}')
+
+# substitution
+censored_review = re.sub(price_regex, '$UNKNOWN', review)
+print(f'censored review = {censored_review}')
+
+# split
+punctuation_regex = re.compile('[.,;:?!]')
+sentence = 'It was the best of times; it was the worst of times. It was '
+result = re.split(punctuation_regex, sentence)
+print(f'result = {result}')
+
+# password rules
+password_regex = re.compile('[a-zA-Z0-9?.<>;:!@&^%$]{8,}')
--- section017/04_regex.py
+++ section017/04_regex.py
@@ -1,2 +1,36 @@
 # regular expressions
 
+# [0123456789]
+# [0-9]
+# [a-z]+
+# [^a-z]
+# [^0123456789]
+# \s - any whitespace (tab, space, newline)
+# \w - any "word"
+# \d - any digit [0-9]
+
+# [a-zA-Z_]\w*
+
+import re 
+
+regex1 = re.compile('\$\d+')
+match1 = regex1.search('I paid $100 for this item.  It was not even worth $1')
+print(f'The result of search() is {match1}')
+if match1:
+    print(f'The string matches!')
+    print(f'The matched string: {match1.group()}')
+
+new_review = re.sub(regex1, '$UNKNOWN', 'I paid $100 for this item.  It was not even worth $1')
+print(f'The censored review is {new_review}')
+
+name_regex = re.compile('[A-Z][a-z]*')
+name_match = name_regex.match('John Jonah Jameson')
+if name_match:
+    print(f'Start index:    {name_match.start()}')
+    print(f'End index:      {name_match.end()}')
+    print(f'Matched string: {name_match.group()}')
+print(f'All names are: {name_regex.findall("John Jonah Jameson")}')
+
+punctuation_regex = re.compile('[.,:;!?]')
+result = re.split(punctuation_regex, 'It was the best of times; it was the worst of times.')
+print(result)
