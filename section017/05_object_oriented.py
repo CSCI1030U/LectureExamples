ec340b260f14329abbae70d7af317efae90f9670
randyfortier <randy.fortier@uoit.ca>
2021-10-25 12:32:52 -0400

    object-oriented for section 017

---
+++ section017/05_object_oriented.py
@@ -0,0 +1,33 @@
+# classes, methods, instance variables, inheritance
+
+class Pet:
+    def __init__(self, name, breed, health, mass):
+        # _ prefix means 'private'
+        self._name = name
+        self._breed = breed
+        self._health = health 
+        self._mass = mass 
+        self._num_visits = 0
+
+    # accessor (read the value of an instance variable)
+    def get_name(self):
+        return self._name
+
+    # mutator (set the value of an instance variable)
+    def set_name(self, new_name):
+        self._name = new_name
+    
+    def feed(self, amount):
+        self._mass += amount
+
+    def __str__(self):
+        return f'Pet({self._name}, {self._breed})'
+
+
+spot = Pet('Spot', 'Poodle', 100, 2.1) # create an instance
+boots = Pet('Boots', 'Domestic Cat', 80, 1.4)
+
+# print(boots._name)   # should never happen (bad practice to access private variables external to the class)
+print(boots.get_name())
+
+print(spot)
