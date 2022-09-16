bdcf2794d915a62b41364060b40b9bdf81e208d6
randyfortier <randy.fortier@uoit.ca>
2022-09-16 10:08:43 -0400

    Added coding challenge solution for this week

---
+++ coding_challenge_solutions/01b_turtle.py
@@ -0,0 +1,46 @@
+import turtle 
+
+window = turtle.Screen()
+t = turtle.Turtle()
+
+# move into position for the R
+t.penup()
+t.right(90)
+t.forward(150) # down
+t.right(90)
+t.forward(200) # left
+
+# draw the R
+t.pendown()
+t.right(90) # up
+t.forward(300)
+t.right(90) # right 
+t.forward(150)
+t.right(90) # down
+t.forward(150)
+t.right(90) # left 
+t.forward(150)
+t.left(135) # diagonal down/right
+t.forward(212)
+
+# move into position for the F
+t.penup()
+t.left(45)
+t.forward(100)
+
+# draw the F
+t.pendown()
+t.left(90)
+t.forward(300)
+t.right(90)
+t.forward(150)
+t.left(180)
+t.forward(150)
+t.left(90)
+t.forward(150)
+t.left(90)
+t.forward(150)
+
+t.penup()
+
+window.mainloop()
