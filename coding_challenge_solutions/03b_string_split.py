35a950d6f1e0da365d3ad812c73ee3ab3a06b16c
randyfortier <randy.fortier@uoit.ca>
2022-09-29 14:41:27 -0400

    Added more challenging solution to exercise 1, with no split() function

--- coding_challenge_solutions/03b_string_split.py
+++ coding_challenge_solutions/03b_string_split.py
@@ -1,3 +1,5 @@
+# coding challenge solution
+
 sentence = 'ahmed runs quickly'
 word = ''
 words = []
@@ -10,3 +12,19 @@ for character in sentence:
 words.append(word) # the final word has no delim
 
 print(f'{words = }')
+
+# more advanced solution for coding exercise 1 (without using split())
+sentence = 'ahmed runs quickly'
+reverse = ''
+current_word = ''
+for character in sentence:
+    if character == ' ':
+        # this is a delimiter, so add the characters in the current word to the reversed sentence
+        reverse = current_word + ' ' + reverse 
+        current_word = ''
+    else:
+        current_word += character
+# add the final word (which has no following delimiter)
+reverse = current_word + ' ' + reverse 
+
+print(f'{reverse.strip() = }')
