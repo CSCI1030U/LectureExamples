7c1079edf9d48dadcf4449a866ac18358bb18c90
randyfortier <randy.fortier@uoit.ca>
2022-10-25 09:18:24 -0400

    Added starter code for this week's exercise

---
+++ extra/05_oop.py
@@ -0,0 +1,115 @@
+class Student:
+    def __init__(self, sid, first_name, last_name):
+        self.sid = sid 
+        self.first_name = first_name
+        self.last_name = last_name
+        self.registered_date = {
+            'year': 2022,
+            'month': 9,
+            'day': 1,
+        }
+    
+    def set_sid(self, new_sid): # mutator (setter)
+        self.sid = new_sid
+
+    def get_sid(self): # accessor (getter)
+        return self.sid    
+
+    def __eq__(self, other):
+        return self.sid == other.sid and self.first_name == other.first_name and self.last_name == other.last_name
+
+    def __str__(self):
+        return f'{self.first_name} {self.last_name} (sid: {self.sid})'
+
+student1 = Student('100000001', 'Giselle', 'LeClerc')
+
+student1_copy = Student('100000001', 'Giselle', 'LeClerc')
+
+student1_string = str(student1)
+print(f'{student1_string = }')
+print(student1 == student1_copy)
+
+class Grad_Student(Student):
+    def __init__(self, sid, first_name, last_name):
+        super().__init__(sid, first_name, last_name)
+        self.supervisor = None
+    
+    def set_supervisor(self, new_supervisor):
+        self.supervisor = new_supervisor
+
+student2 = Grad_Student('100000002', 'Rhonda', 'Brown')
+student2.registered_date = {'year': 2023, 'month': 1, 'day': 1}
+
+student2_copy = Grad_Student('100000002', 'Rhonda', 'Brown')
+
+print(f'{str(student2) = }')
+print(student2.registered_date)
+print(student2 == student2_copy)
+
+# exercise 1 (generator)
+
+class Square_Generator:
+    def __init__(self, start_num, end_num):
+        self.next_num = start_num 
+        self.end_num = end_num 
+    
+    def __iter__(self):
+        return self 
+    
+    def __next__(self):
+        if self.next_num < self.end_num:
+            current_num = self.next_num * self.next_num
+            self.next_num += 1
+            return current_num
+        raise StopIteration()
+
+for square in Square_Generator(5, 10):
+    print(f'{square = }')
+
+print(f'{list(Square_Generator(5, 10)) = }')
+
+# exercise 2 (Shoe, Product)
+
+class Product:
+    def __init__(self, price, description):
+        self.price = price 
+        self.description = description
+    
+    def get_price(self):
+        return self.price 
+    
+    def get_description(self):
+        return self.description
+
+class Shoe(Product):
+    def __init__(self, brand, size, colour, price, description):
+        super().__init__(price, description)
+
+        self.brand = brand 
+        self.size = size 
+        self.colour = colour
+    
+    def get_brand(self):
+        return self.brand 
+
+    def get_size(self):
+        return self.size 
+
+    def get_colour(self):
+        return self.colour 
+    
+    def __lt__(self, other):
+        return self.size < other.size
+
+    def __gt__(self, other):
+        return self.size > other.size
+
+    def __str__(self):
+        return f'{self.brand} (size: {self.size}, colour: {self.colour}, price: {self.price})'
+
+shoe1 = Shoe('Nike', 11, 'White', 189.99, 'Jordan Delta 3')
+shoe2 = Shoe('Adidas', 13, 'White', 99.99, 'Grand Court Tennis')
+
+print(f'{str(shoe1) = }')
+print(f'{str(shoe2) = }')
+print(shoe1 < shoe2)
---
+++ section001/06_regex_fsas.py
@@ -0,0 +1,209 @@
+# reference
+
+# regular expressions
+
+# ..   - match any two characters
+# a|b  - matches a, b
+# (a|b)(a|b) - matches aa, ab, ba, bb
+# (a|b)*     - match <blank>, a, b, aa, ab, ba, bb, aaa, aab, ...
+
+
+# a+                 - matches a, aa, aaa, aaaa, ...
+# (a|b)+             - matches a, b, aa, ab, ba, bb, aaa, aab, aba, ...
+# (a|b+)             - matches a, b, bb, bbb, bbbb, ...
+# (a|b*)             - matches a, <blank>, b, bb, bbb, bbbb, ...
+# (a+|b+)            - matches a, aa, aaa, aaaa, ..., b, bb, bbb, bbbb, ...
+# a?b+               - matches ab, abb, abbb, abbbb, ..., b, bb, bbb, bbbb, ...
+# a{7}               - matches aaaaaaa (only)
+# (a|b){4}           - matches aaaa, aaab, aaba, aabb, abaa, ...
+# (a|b){2,4}         - matches aa, ab, ba, bb, aaa, aab, aba, ..., aaaa, aaab, aaba, ...
+# (a|b){2}|(a|b){4}  - matches aa, ab, ba, bb, aaaa, aaab, aaba, aabb, ...
+
+# (a|b|c|d|e|f|g|h|i|j|k|l|m|n|o|p|q|r|s|t|u|v|w|x|y|z)  - long and inconvenient, so we need shortcuts
+# [abcdefghijklmnopqrstuvwxyz]
+# [a-z]      - matches a, b, c, d, e, f, ..., z
+# [0-9]      - matches 0, 1, 2, 3, 4, 5, 6, 7, 8, 9
+# \d         - any digit character (as above)
+# [a-zA-Z]   - matches a, b, c, d, e, f, ..., z, A, B, C, D, E, F, ..., Z
+# [a-zA-Z]+  - matches a, A, b, B, c, C, d, D, ..., aa, aA, ab, aB, ...
+# [abc]      - matches a, b, c
+# [^abc]     - matches any character, except a, b, and c
+# [^0-9]     - matches any non-digit character
+# [.,;:?!]   - matches any punctuation character
+# \w         [a-zA-Z0-9_]  - any character that can be in a variable name
+# \s         - matches space, tab, or newline
+
+# [a-z_]+    - matches snake case only
+# [a-zA-Z]+  - matches camel case only
+# [a-zA-Z0-9_&^%$]{8,}      - matches any sequence of 8 or more of those characters
+
+
+# practice
+
+
+
+# coding exercise - recognize a binary number (sequence of 1s and 0s) 8 or 16 chars long
+
+
+
+# coding exercise (John Conway's Game of Life)
+
+from curses import wrapper
+import curses
+import time
+
+def draw(stdscr, world):
+    stdscr.clear()
+    for y in range(len(world)):
+        for x in range(len(world[y])):
+            if world[y][x] == 1:
+                stdscr.addstr(y, x, ' ', curses.color_pair(1))
+    stdscr.refresh()
+
+def _is_valid_coord(world, y, x):
+    return x >= 0 and y >= 0 and y < len(world) and x < len(world[y])
+    # if x >= 0 and y >= 0 and x < len(world[y]) and y < len(world):
+    #     return True
+    # else:
+    #     return False
+
+def _get_neighbours(world, y, x):
+    neighbours = []
+
+    # 000
+    # 0X0
+    # 000
+
+    # get the neighbours from the row above
+    if _is_valid_coord(world, y - 1, x - 1):
+        neighbours.append(world[y - 1][x - 1])
+    if _is_valid_coord(world, y - 1, x):
+        neighbours.append(world[y - 1][x])
+    if _is_valid_coord(world, y - 1, x + 1):
+        neighbours.append(world[y - 1][x + 1])
+
+    # get the neighbours from the same row
+    if _is_valid_coord(world, y, x - 1):
+        neighbours.append(world[y][x - 1])
+    if _is_valid_coord(world, y, x + 1):
+        neighbours.append(world[y][x + 1])
+
+    # get the neighbours from the row below
+    if _is_valid_coord(world, y + 1, x - 1):
+        neighbours.append(world[y + 1][x - 1])
+    if _is_valid_coord(world, y + 1, x):
+        neighbours.append(world[y + 1][x])
+    if _is_valid_coord(world, y + 1, x + 1):
+        neighbours.append(world[y + 1][x + 1])
+
+    return neighbours
+
+def _num_alive_neighbours(world, y, x):
+    neighbours = _get_neighbours(world, y, x)
+    return neighbours.count(1)
+
+def update(world):
+    new_world = []
+
+    # TODO: Add update code
+
+    return new_world
+
+def main(stdscr):
+    width = curses.COLS
+    height = curses.LINES
+    # print(f'screen dimensions: {width}, {height}')
+
+    curses.curs_set(0)
+    curses.init_pair(1, curses.COLOR_BLACK, curses.COLOR_WHITE)
+
+    # pulsar
+    # world = [
+    #     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    #     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    #     [0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0],
+    #     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    #     [0, 0, 1, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 1, 0, 0],
+    #     [0, 0, 1, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 1, 0, 0],
+    #     [0, 0, 1, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 1, 0, 0],
+    #     [0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0],
+    #     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    #     [0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0],
+    #     [0, 0, 1, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 1, 0, 0],
+    #     [0, 0, 1, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 1, 0, 0],
+    #     [0, 0, 1, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 1, 0, 0],
+    #     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    #     [0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0],
+    #     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    #     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    # ]
+
+    # static (square), oscillator (+), glider
+    world = [
+        [0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 1, 0],
+        [1, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 1, 0],
+        [0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    ]
+
+    # Gosper's glider gun
+    # world = [
+    #     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    #     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    #     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    #     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0],
+    #     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0],
+    #     [0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    #     [0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 1, 1, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    #     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    #     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    #     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    #     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    #     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    #     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    #     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    #     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    #     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    #     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    #     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    #     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    #     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    #     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    #     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    #     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    #     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    #     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    #     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    #     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    #     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    #     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    #     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    # ]
+
+    draw(stdscr, world)
+
+    frame_delay = 0.1
+    iterations = 200
+
+    while iterations > 0:
+        time.sleep(frame_delay)
+        world = update(world)
+        draw(stdscr, world)
+        iterations -= 1
+
+    stdscr.getkey()
+
+wrapper(main)
